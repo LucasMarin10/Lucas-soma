6defb5c6106f3f6c8da536ab2655a48b6c5155d5
LucasMarin10 <84347671+LucasMarin10@users.noreply.github.com>
2021-06-02 15:40:01 -0300

    Add files via upload

---
+++ Bottle_of_Lucas.py
@@ -0,0 +1,37 @@
+FF=["Omelete","Yakisoba","Pasta","Pizza","Hamburger", "Miojo"]
+print(FF)
+list2=sorted(FF)
+print(list2)
+print(FF)
+FF.sort()
+print(FF)
+name=["L","U","C","A","S"]
+print(name)
+name.reverse()
+print(name)
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
+print(len(FF))
+print(FF[0].upper())
+for Food in FF:
+    print("Begin of loop")
+    print(Food.upper())
+    print("End of loop")
+print("After loop")
+numbers=[1,2,3,4,5,6]
+for number in numbers:
+    print(number)
+print("lucas")
+listR=["bananas","dogs","bottles","cups"]
+for RandomL in listR:
+    print(RandomL)
+    print(listR)
+print("****************")
+#numbersC=[1,2,3,4,5,6,7,8,9,10]
+numbersC=[1,2,3,4,5,6,7,8,9,10]
+soma = 0
+for numbe in numbersC:
+    soma = soma + numbe
+print("lucas calculated this: " + str(soma))
+    
+    
